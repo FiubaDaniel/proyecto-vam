f5663a641b3b3b476e94000fe7c1bca7748ef7f1
FiubaDaniel <FiubaDaniel@gmail.com>
2012-01-02 01:56:23 +0000

    Modificaciones en todas las clases del modelo.
    Cambios importantes en la maquina de estado.

--- Vam/src/main/java/com/daniel/model/Cliente.java
+++ Vam/src/main/java/com/daniel/model/Cliente.java
@@ -2,4 +2,32 @@ package com.daniel.model;
 
 public class Cliente {
 
+	private String nombre;
+	private String apellido;
+	private long dni;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
 }
--- Vam/src/main/java/com/daniel/model/Operadora.java
+++ Vam/src/main/java/com/daniel/model/Operadora.java
@@ -2,4 +2,55 @@ package com.daniel.model;
 
 public class Operadora {
 
+	private String nombre;
+	private String apellido;
+	private long idDeOperadora;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getIdDeOperadora() {
+		return idDeOperadora;
+	}
+
+	public void setIdDeOperadora(long idDeOperadora) {
+		this.idDeOperadora = idDeOperadora;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ (int) (idDeOperadora ^ (idDeOperadora >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Operadora other = (Operadora) obj;
+		if (idDeOperadora != other.idDeOperadora)
+			return false;
+		return true;
+	}
+
 }
